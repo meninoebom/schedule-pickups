f49236ee3f77d0e45a4edc72d506d13d55f8360b
Brandon Brown <brandon.brown@ymail.com>
2015-08-18 17:35:59 -0700

    implements a timepicker for the menu view

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-var App = angular.module( 'schedulePickups', ['ui.router', 'firebase'] );
+var App = angular.module( 'schedulePickups', ['ui.router', 'ui.bootstrap', 'firebase'] );
 
 App.config(function( $stateProvider, $urlRouterProvider ){
   $urlRouterProvider.otherwise('/');
@@ -37,18 +37,20 @@ App.controller('HomeCtrl', function( $scope, $rootScope, $sce, $firebaseArray ) 
 
 });
 
-App.controller('MenuCtrl', function( $scope, $stateParams, $firebaseArray, restaurantService ){
+App.controller('MenuCtrl', function( $scope, $stateParams, $firebaseArray, restaurantService, $log ){
   $scope.data = restaurantService.getRestaurant($stateParams.restaurantId);
+  
   $scope.order = {
     pickupTime: "",
     menuItems: {},
     pastDue: false
   };
+
   for(menuItemId in $scope.data.menuItems) {
     $scope.order.menuItems[menuItemId] = 0;
   }
-  var customer = '123';
-  var restaurantId = $stateParams.restaurantId;
+
+  var customer = '123', restaurantId = $stateParams.restaurantId;
   var fbRef = new Firebase("https://fiery-inferno-5692.firebaseio.com/"+customer+"/orders/"+restaurantId);
   $scope.createOrder = function() {
     console.log($scope.order);
@@ -56,6 +58,36 @@ App.controller('MenuCtrl', function( $scope, $stateParams, $firebaseArray, resta
     //   foo: "bar"
     // });
   }
+
+$scope.mytime = new Date();
+
+  $scope.hstep = 1;
+  $scope.mstep = 15;
+
+  $scope.options = {
+    hstep: [1, 2, 3],
+    mstep: [1, 5, 10, 15, 25, 30]
+  };
+
+  $scope.ismeridian = true;
+  $scope.toggleMode = function() {
+    $scope.ismeridian = ! $scope.ismeridian;
+  };
+
+  $scope.update = function() {
+    var d = new Date();
+    d.setHours( 14 );
+    d.setMinutes( 0 );
+    $scope.mytime = d;
+  };
+
+  $scope.changed = function () {
+    $log.log('Time changed to: ' + $scope.mytime);
+  };
+
+  $scope.clear = function() {
+    $scope.mytime = null;
+  };
    
 })
 
